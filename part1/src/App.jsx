dadb5753748c84978bad7fa5246fc6dc852434a9
Wes <piuswesley@gmail.com>
2023-08-04 14:53:59 +0800

    Finish ex 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,11 @@
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
+    );
+};
+
 const Header = ({ course }) => {
     return <h1>{course}</h1>;
 };
@@ -5,15 +13,9 @@ const Header = ({ course }) => {
 const Content = (props) => {
     return (
         <>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
+            <Part part={props.part1} exercises={props.exercise1}></Part>
+            <Part part={props.part2} exercises={props.exercises2}></Part>
+            <Part part={props.part3} exercises={props.exercises3}></Part>
         </>
     );
 };
