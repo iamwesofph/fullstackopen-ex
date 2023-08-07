5926148b2692f1c23ba1bafb58fe3e600069c2dd
Wes <piuswesley@gmail.com>
2023-08-07 22:25:45 +0800

    Finish phonebook step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ handleChangeSearch, search }) => {
     return (
@@ -28,7 +30,6 @@ const PersonForm = ({ newName, newNumber, handleChangeName, handleChangeNumber, 
 
 const Persons = ({ persons, search }) => {
     const personsToShow = persons.filter((person) => person.name.includes(search));
-
     return (
         <ul>
             {personsToShow.map((person, index) => {
@@ -38,18 +39,29 @@ const Persons = ({ persons, search }) => {
     );
 };
 
-const initialPersons = [
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-];
+// const initialPersons = [
+//     { name: "Arto Hellas", number: "040-123456", id: 1 },
+//     { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+//     { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+//     { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+// ];
 
 const App = () => {
-    const [persons, setPersons] = useState(initialPersons);
+    // const [persons, setPersons] = useState(initialPersons);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        console.log("effect");
+        axios.get("http://localhost:3001/persons").then((response) => {
+            console.log("promise fulfilled");
+            setPersons(response.data);
+        });
+    }, []);
+
+    console.log("render", persons.length, "persons");
 
     const handleChangeName = (event) => {
         setNewName(event.target.value);
@@ -75,7 +87,6 @@ const App = () => {
         if (existingPerson) {
             alert(`${newName} is already added to phonebook`);
         } else {
-            alert("new person created");
             setPersons([...persons, newPerson]);
             setNewName("");
             setNewNumber("");
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,9 +1,23 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+// const promise2 = axios.get("http://localhost:3001/persons");
+// console.log(promise2);
+
+// axios.get("http://localhost:3001/persons").then((response) => {
+//     const notes = response.data;
+//     console.log(notes);
+// });
+
+// axios.get("http://localhost:3001/persons").then((response) => {
+//     const notes = response.data;
+//     ReactDOM.createRoot(document.getElementById("root")).render(<App notes={notes} />);
+// });
