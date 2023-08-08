8e8584aff476c38f3e25491b580ed73239ebbe79
Wes <piuswesley@gmail.com>
2023-08-08 20:54:47 +0800

    Add new project dataforcountries

---
+++ part2/dataforcountries/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <>
+            <p>Find Countries</p>
+            <input type="text" />
+        </>
+    );
+}
+
+export default App;
---
+++ part2/dataforcountries/src/services/persons.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+    const request = axios.get(baseUrl);
+    return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject);
+    return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject);
+    return request.then((response) => response.data);
+};
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`);
+    return request.then((response) => {
+        console.log("Status Code:", response.status); // Logging the status code
+        console.log("Data:", response.data); // Logging the status code
+        return response.data;
+    });
+};
+
+export default {
+    getAll,
+    create,
+    update,
+    remove,
+};
