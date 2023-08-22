7bccb5e0dd4ab16e2b4fde7f26c26f9a1c8ec953
iamwesofph <piuswesley@gmail.com>
2023-08-22 10:59:31 +0800

    Finish bloglist expansion step5

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,16 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
     response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
     const body = request.body;
+    const user = await User.findOne({});
+    console.log(user.id);
 
     if (!body.title || !body.url) {
         return response.status(400).json({ error: "Title and URL are required" });
@@ -18,10 +21,14 @@ blogsRouter.post("/", async (request, response) => {
         author: body.author,
         url: body.url,
         likes: body.likes || 0,
+        user: user.id,
     });
 
-    const result = await blog.save();
-    response.status(201).json(result);
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
