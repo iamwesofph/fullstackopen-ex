f29e6922e21515d13bb2b5c02295e133ad8e7444
iamwesofph <piuswesley@gmail.com>
2023-08-22 19:27:53 +0800

    Finish bloglist expansions step7

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+        return authorization.replace("Bearer ", "");
+    }
+    return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
     const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -9,8 +18,13 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
     const body = request.body;
-    const user = await User.findOne({});
-    console.log(user.id);
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: "token invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
 
     if (!body.title || !body.url) {
         return response.status(400).json({ error: "Title and URL are required" });
